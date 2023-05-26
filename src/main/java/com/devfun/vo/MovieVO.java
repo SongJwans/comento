20c66d0fccc9890555043aaa560fb3951b5655ad
songjuhwan <songjuhwan@175.119.120.111>
2023-05-26 22:45:36 +0900

    송주환 1차 과제

---
+++ src/main/java/com/devfun/vo/MovieVO.java
@@ -0,0 +1,36 @@
+package com.devfun.vo;
+
+public class MovieVO {
+
+	String movie_name;
+	String director;
+	String types;
+	
+
+	public MovieVO(String movie_name, String director, String types) {
+		super();
+		this.movie_name = movie_name;
+		this.director = director;
+		this.types = types;
+	}
+	
+	public String getMovie_name() {
+		return movie_name;
+	}
+	public void setMovie_name(String movie_name) {
+		this.movie_name = movie_name;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	public String getTypes() {
+		return types;
+	}
+	public void setTypes(String types) {
+		this.types = types;
+	}
+
+}
